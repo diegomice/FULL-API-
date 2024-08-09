2f4051b9ef34daaaf6d9754ca417fbfe3423d6cd
Diego Mice <168139215+diegomice@users.noreply.github.com>
2024-08-08 21:42:20 -0500

    componentes: manageorders y modalorders,estilos

--- carritoBasicAvanzado-main/src/components/Header.jsx
+++ carritoBasicAvanzado-main/src/components/Header.jsx
@@ -51,7 +51,7 @@ const Header = ({ cartCount }) => {
         <Link to="/sales-report">Reporte de Ventas</Link>
         {user.role === 'admin' && <Link to="/manage-products">G. Productos</Link>}
         {user.role === 'admin' && <Link to="/manage-orders">G. Pedidos</Link>} {/* Link a la gestión de pedidos */}
-        {user.username ? (
+     {user.username ? (
           <>
             <Link to="/pedidos">Mis Pedidos</Link> {/* Enlace a los pedidos */}
             <span>  <Link to="/update-user">Bienvenido, {user.username} ({user.role})</Link></span>
